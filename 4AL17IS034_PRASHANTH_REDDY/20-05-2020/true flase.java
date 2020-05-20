1bb390f5e8a63bc10d026131caf7c901d70a9560
PRASHANTH REDDY <prashanthreddy1040@gmail.com>
2020-05-20 19:15:08 +0530

    DAY 3

---
+++ 4AL17IS034_PRASHANTH_REDDY/20-05-2020/true flase.java
@@ -0,0 +1,43 @@
+In this challenge, we test your knowledge of using if-else conditional statements to automate decision-making processes. An if-else statement has the following logical flow:
+
+Wikipedia if-else flow chart
+
+Source: Wikipedia
+
+Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
+
+
+Solution:
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+    }
+}
+
+
+
