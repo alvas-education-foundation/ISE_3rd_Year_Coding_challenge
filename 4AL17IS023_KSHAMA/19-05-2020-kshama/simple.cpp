f9e6be11bc1133fffae8edec575ebe0913fc5363
kshamatantri <kshamatantri@gmail.com>
2020-05-20 12:50:20 +0530

    uploaded programs

---
+++ 4AL17IS023_KSHAMA/19-05-2020-kshama/simple.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  //Type your code here.
+  int w,x,y,z=0;
+  std::cin>>w;
+  std::cin>>x;
+  std::cin>>y;
+  z=((w*x)-(w*y))-100;
+  std::cout<<z;
+}
