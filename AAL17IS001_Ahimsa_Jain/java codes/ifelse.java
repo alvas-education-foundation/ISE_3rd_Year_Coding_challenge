29d8446008488400190cdf25553125bedc969388
ahimsajain29 <ahimsa.jain2000@gmail.com>
2020-05-20 12:00:55 +0530

    added a java code.

--- AAL17IS001_Ahimsa_Jain/java codes/ifelse.java
+++ AAL17IS001_Ahimsa_Jain/java codes/ifelse.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution //main class
+public class Solution
 {    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
