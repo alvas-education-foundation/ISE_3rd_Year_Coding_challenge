b0017b8608d2fe30984e1ee0bd79eb5b0125e3d3
VishakAmin <aminvishak@gmail.com>
2020-05-21 16:31:47 +0530

    Adde Day 3

---
+++ 4AL17IS006_VISHAK_AMIN/21May20/1.py
@@ -0,0 +1,22 @@
+# Task
+# Read an integer . For all non-negative integers , print . See the sample for details.
+
+# Input Format
+
+# The first and only line contains the integer, .
+
+# Constraints
+
+
+# Output Format
+
+# Print  lines, one corresponding to each .
+
+def calc(n):
+    for i in range(0,n):
+        print(i*i)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    calc(n)
