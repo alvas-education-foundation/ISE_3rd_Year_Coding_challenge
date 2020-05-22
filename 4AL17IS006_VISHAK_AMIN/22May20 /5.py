49156013ba5803df183baaed0c52cb0ecd3dc5e7
VishakAmin <aminvishak@gmail.com>
2020-05-22 22:16:48 +0530

    Added Day 5 Codes

---
+++ 4AL17IS006_VISHAK_AMIN/22May20 /5.py
@@ -0,0 +1,18 @@
+# You have a record of  students. Each record contains the student's name, and their percent marks in Maths, Physics and Chemistry. The marks can be floating values. The user enters some integer  followed by the names and marks for  students. You are required to save the record in a dictionary data type. The user then enters a student's name. Output the average percentage marks obtained by that student, correct to two decimal places.
+
+
+marks= []
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    l = list(student_marks[query_name])
+    n1 = len(l)
+    s = sum(l)
+    total = s/n1
+    print("%.2f"%total)
