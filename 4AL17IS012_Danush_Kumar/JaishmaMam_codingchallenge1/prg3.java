a0a55aad2b99d2d7030a1cb9e9c4c0d442352683
Danush Kumar <dhanushkotian24@gmail.com>
2020-05-26 20:38:39 +0530

    updated

--- 4AL17IS012_Danush_Kumar/JaishmaMam_codingchallenge1/prg3.java
+++ 4AL17IS012_Danush_Kumar/JaishmaMam_codingchallenge1/prg3.java
@@ -37,7 +37,7 @@ class Solution{
                     if(x>=–128 && x<=127) System.out.println(“* byte”);
                     if(x>=-32768 && x<=32767) System.out.println(“* short”);
                     if(x>=-2147483648 && x<=2147483647) System.out.println(“* int”);
-                    if(x>=-9223372036854775808 &&
+                    if(x>=-9223372036854775808  &&
                         x<=9223372036854775807)System.out.println(“* long”);
                }
                catch(Exception e) {
