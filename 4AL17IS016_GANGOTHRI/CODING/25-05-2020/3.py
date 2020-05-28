6debcf131c4c76220ba994ade1e83f90d10dc76d
gangothrivijay <44720536+gangothrivijay@users.noreply.github.com>
2020-05-28 09:31:50 +0530

    Delete 3.py

--- 4AL17IS016_GANGOTHRI/CODING/25-05-2020/3.py
+++
@@ -1,9 +0,0 @@
-# Task
-# Given an integer, , and  space-separated integers as input, create a tuple, , of those  integers. Then compute and print the result of .
-
-# Note: hash() is one of the functions in the __builtins__ module, so it need not be imported.
-
-if __name__ == '__main__':
-    n = int(input())
-    integer_list = tuple(map(int, input().split()))
-    print(hash(integer_list))
