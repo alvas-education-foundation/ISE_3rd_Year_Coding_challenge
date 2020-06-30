586c2372a34d2dcc223f07f7447c16c0217ee007
PRASHANTH REDDY <prashanthreddy1040@gmail.com>
2020-06-30 12:04:42 +0530

    SK and PN coding challenge

---
+++ 4AL17IS034_PRASHANTH_REDDY/Pre_Placement_Training_Day_4 (18_06_2020)/PN_coding-challenge (30_06_2020)/find_sum.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+#include<math.h>
+
+void main()
+{
+clrscr(); //to clear the screen
+long i,n,x,sum=1;
+
+cout<<“1+x+x^2+……+x^n”;
+cout<<“nnEnter the value of x and n:”;
+cin>>x>>n;
+
+for(i=1;i<=n;++i)
+sum+=pow(x,i);
+cout<<“nSum=”<<sum;
+getch(); //to stop the screen
+}
